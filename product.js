046d352be512c074d275f4f3c36d61d1d3feea05
GAMING X LITE <real012300@gmail.com>
2026-01-16 19:59:15 +0530

    product.js

---
+++ product.js
@@ -0,0 +1,32 @@
+const PRODUCTS = [
+  {
+    badge: "BASIC",
+    name: "GUILD GLORY (100K)",
+    price: 300,
+    features: ["Instant Delivery","Secure UPI Payment","Manual Verification","4 Bots"]
+  },
+  {
+    badge: "💥BEST SELLER💥",
+    name: "GUILD GLORY BOT(300K)",
+    price: 500,
+    features: ["Instant Delivery","Secure UPI Payment","Manual Verification","8 Bots"]
+  },
+  {
+    badge: "⭐MOST VALUE⭐",
+    name: "GUILD GLORY BOT (500K)",
+    price: 700,
+    features: ["Instant Delivery","Secure UPI Payment","Manual Verification","12 Bots"]
+  },
+  {
+    badge: "🔥HOT🔥",
+    name: "GUILD GLORY BOT (900K)",
+    price: 1000,
+    features: ["Instant Delivery","Secure UPI Payment","Manual Verification","16 Bots"]
+  },
+  {
+    badge: "💎PREMIUM💎",
+    name: "CARD (8K)",
+    price: 1000,
+    features: ["Instant Delivery","Secure UPI Payment","Manual Verification"]
+  },
+];
